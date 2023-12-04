06db3969692d10b27c6e1efd26c2051fc859e5cc
shikhar2817 <shikharsingh0076@gmail.com>
2023-12-04 22:05:41 +0530

    initial setup

---
+++ server/src/index.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import cors from "cors";
+import { PORT } from "./env";
+import expressConfig from "./express.config";
+
+const app = expressConfig();
+
+// CORS SETUP
+
+app.use(
+    cors({
+        credentials: true,
+    })
+);
+
+app.listen(PORT, () => console.log("Server Running on Port " + PORT));
+
+app.get("/api/health", (_req: Request, res: Response) => {
+    try {
+        res.status(200).json({ message: "System is healthy 🟢" });
+    } catch (error: unknown) {
+        console.log("Server Failed: ", error);
+        res.status(500).json({ message: "System is Sick 🔴" });
+    }
+});
