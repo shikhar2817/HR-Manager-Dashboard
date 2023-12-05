ea57952921c4b50ef7a902b44be5b7e6cc075493
shikhar2817 <shikharsingh0076@gmail.com>
2023-12-05 17:05:21 +0530

    added login signup layout  pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,26 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import { Home } from "./pages/Home";
+import NoPage from "./pages/Nopage";
+import Layout from "./pages/Layout";
 
 function App() {
-    return <>App</>;
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={<Home />} />
+                        <Route path="/signup" element={<SignUp />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="*" element={<NoPage />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
